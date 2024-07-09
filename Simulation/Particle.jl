a446071708991d6c65835ad0dd07d8ebb128b096
Aryan Malhotra <maddyyy467@gmail.com>
2024-07-09 16:38:54 +0530

    Updating Particle into a Module. Vector Support is yet to be added using Static Vectors

--- Simulation/Particle.jl
+++ Simulation/Particle.jl
@@ -1,7 +1,12 @@
+module MyParticle
+export Particle, UnitSphere
+
+
+
 include("./MyColors.jl")
 using GeometryBasics, .MyColors, Observables
 
-const UnitSphere = normal_mesh(Tesselation(Sphere(Point3f(0), 1), 50)) 
+const UnitSphere = normal_mesh(Tesselation(Sphere(Point3f(0), 1), 50))
 #You can also use Sphere() as a marker but I like this.
 #mesh() would plot a mesh in Makie. GeometryBasics.mesh creates some non-colorable version of the object. normal_mesh is the way to go then.
 
@@ -17,9 +22,9 @@ All properties that affect render are `Observables` by default
 # Fields
 
     #Kinetics
-  -  `position` :: A `Point2` or `Point3`
-  -  `velocity` :: A `Point2` or `Point3`
-  -  `acceleration` :: A `Point2` or `Point3`
+  -  `position` :: A `Point2` or `Point3` or `Tuple`
+  -  `velocity` :: A `Point2` or `Point3` or `Tuple`
+  -  `acceleration` :: A `Point2` or `Point3` or `Tuple`
 
     #Intrinsic Properties
   -  `mass` :: `Real`
@@ -80,19 +85,39 @@ mutable struct Particle
     shape
     size
     alpha
-
-    function Particle(position::Point3=Point3f(0), velocity::Point3=Point3f(0), acceleration::Point3=Point3f(0);
+    #Observable{<:Point3{<:Real}} might add this as well
+    function Particle(
+        position::Union{Point3{<:Real},Tuple{Real,Real,Real}}=Point3f(0),
+        velocity::Union{Point3{<:Real},Tuple{Real,Real,Real}}=Point3f(0),
+        acceleration::Union{Point3{<:Real},Tuple{Real,Real,Real}}=Point3f(0);
         mass::Real=1, charge::Real=1, color::String=rand(BASE_COLORS),
         shape=UnitSphere,
-        size::Real=5, alpha::Real=1)
-        new(Observable(position), velocity, acceleration, mass, charge, Observable(color), Observable(shape), Observable(size), Observable(alpha))
+        size::Real=1, alpha::Real=1
+    )
+        new(
+            Observable(Point(position)), Point(velocity), Point(acceleration),
+            mass, charge, Observable(color), Observable(shape), Observable(size), Observable(alpha)
+        )
     end
 
-    function Particle(position::Point2=Point2f(0), velocity::Point2=Point2f(0), acceleration::Point2=Point2f(0);
+    function Particle(
+        position::Union{Point2{<:Real},Tuple{Real,Real}}=Point2f(0),
+        velocity::Union{Point2{<:Real},Tuple{Real,Real}}=Point2f(0),
+        acceleration::Union{Point2{<:Real},Tuple{Real,Real}}=Point2f(0);
         mass::Real=1, charge::Real=1, color::String=rand(BASE_COLORS),
-        shape=GeometryBasics.Circle, size::Real=10, alpha::Real=1)
-        new(Observable(position), velocity, acceleration, mass, charge, Observable(color), Observable(shape), Observable(size), Observable(alpha))
+        shape=GeometryBasics.Circle, size::Real=10, alpha::Real=1
+    )
+        new(
+            Observable(Point(position)), Point(velocity), Point(acceleration),
+            mass, charge, Observable(color), Observable(shape), Observable(size), Observable(alpha)
+        )
     end
 end
 
 #Note: If Alpha != 1, set transparency = true else things dont really work as expected
+
+
+
+
+#END OF MODULE
+end
---
+++ Simulation/polar_coords.jl
@@ -0,0 +1,92 @@
+#Coords Conversion
+"""
+    c2p(x::Real=0, y::Real=0, z::Real=0; convention=:math)
+    
+Converts Cartesian-coordinates (x, y, z) to Polar coordinates (r, θ, ϕ)
+
+# Convention (takes a Keyword Argument)
+- The azimuthal angle (`θ` in `:math`; `ϕ` in `:physics`) is measured between the orthogonal projection of the radial line r onto the reference x-y-plane"
+
+- The polar angle (`ϕ` in `:math`; `θ` in `:physics`) is measured between the z-axis and the radial line r. 
+
+** I use the Geographical Convention (`convention=:math`) by default:
+
+- (radial distance, azimuthal angle, polar angle) -> `(r, θ, ϕ)`
+
+
+** In order to use the ISO convention, set `convention=:physics` for:
+    
+- (radial distance, polar angle, azimuthal angle)  -> `(r, θ, ϕ)`
+
+
+
+# Example
+```jldoctest
+julia> c2p(1, 2, 1)
+3-element Vector{Float64}:
+ 2.449489742783178
+ 1.1071487177940904
+ 1.1502619915109316
+```
+
+Resources: 
+
+https://en.wikipedia.org/wiki/Spherical_coordinate_system
+
+https://youtube.com/watch?v=sT8JIn7Q_Fo
+"""
+function c2p(x::Real=0, y::Real=0, z::Real=0; convention=:math)
+
+    r = round(sqrt(x^2 + y^2 + z^2), digits=10)
+
+    if r == 0
+        return [0, 0, 0]
+    else
+        θ = round(atan(y, x), digits=10) 
+        ϕ = round(atan(sqrt(x^2+y^2), z), digits=10)
+
+        if convention == :physics
+            θ, ϕ = ϕ, θ
+        end
+
+        return [r, θ, ϕ]
+    end
+end
+
+function c2p(point::Union{Tuple, AbstractVector}; convention=:math)
+    c2p(point..., convention=convention)
+end
+
+"""
+    p2c(r::Real=0, θ::Real=0, ϕ::Real=pi/2; convention=:math)
+
+Converts Polar coordinates (r, θ, ϕ) to Cartesian-coordinates (x, y, z) 
+
+# Convention (takes a Keyword Argument)
+- The azimuthal angle (`θ` in `:math`; `ϕ` in `:physics`) is measured between the orthogonal projection of the radial line r onto the reference x-y-plane"
+
+- The polar angle (`ϕ` in `:math`; `θ` in `:physics`) is measured between the z-axis and the radial line r. 
+
+** I use the Geographical Convention (`convention=:math`) by default:
+
+- (radial distance, azimuthal angle, polar angle) -> `(r, θ, ϕ)`
+
+
+** In order to use the ISO convention, set `convention=:physics` for:
+    
+- (radial distance, polar angle, azimuthal angle)  -> `(r, θ, ϕ)`
+
+"""
+function p2c(r::Real=0, θ::Real=0, ϕ::Real=pi/2; convention=:math)
+    if convention == :physics
+        θ, ϕ = ϕ, θ
+    end
+    x = round(r * sin(ϕ) * cos(θ), digits=10)
+    y = round(r * sin(ϕ) * sin(θ), digits=10)
+    z = round(r * cos(ϕ), digits=10)
+
+    return [x, y, z]
+end
+function p2c(point::Union{Tuple, AbstractVector}; convention=:math)
+    p2c(point..., convention=convention)
+end
